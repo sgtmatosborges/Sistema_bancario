cb9f92166756518a4d85d7ee6ba8350afc28fe21
matosborges39 <73673323+sgtmatosborges@users.noreply.github.com>
2025-02-26 17:00:44 -0300

    Inicio_menu_declaracao

---
+++ projeto/sistema_bancario.py
@@ -0,0 +1,17 @@
+menu = """
+==============Menu De Opções Do Banco============
+
+Escolha uma opção:
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] sair
+
+==================================================
+"""
+saldo = 0
+limite = 500
+extrato =" "
+numero_de_saques = 0
+LIMITE_SAQUES = 3
+
